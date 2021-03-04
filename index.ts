9f732a418f593be6cb8eb22afba93fb03caa2f27
Harsh Bhatia <harshb.work@gmail.com>
2021-03-04 16:25:00 +0530

    Added job status

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import * as dotenv from "dotenv";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import fetch from "node-fetch";
 
 dotenv.config();
 
-const { PORT = 9999 } = process.env;
+const { PORT, API_URL, API_TOKEN } = process.env;
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -21,7 +22,6 @@ io.on("connection", (socket: Socket) => {
   console.log(socket.id);
   socket.on("job-progress", (data) => io.emit("job-progress", data));
   socket.on("job-logs", (data) => io.emit("job-logs", data));
-
 });
 
 // server status
@@ -29,5 +29,32 @@ setInterval(function () {
   io.emit("server-status", "I'm fine 🔥");
 }, 2000);
 
+setInterval(function () {
+  getPendingAndErrorJobs().then((result) => {
+    io.emit("job-status", result);
+  });
+}, 2000);
+
 console.log("💡 Socket server up on: " + PORT);
-httpServer.listen(PORT);
+// httpServer.listen(PORT);
+
+const getPendingAndErrorJobs = async () => {
+  const result = {};
+
+  const response = await fetch(
+    `${API_URL}/jobs?size=9999&fields=state&state[]=!finished`,
+    { headers: { Authorization: `Bearer ${API_TOKEN}` } }
+  );
+  const json = await response.json();
+
+  json.data.map((j) => {
+    if (j.state in result) {
+      result[j.state] += 1;
+    } else {
+      result[j.state] = 1;
+    }
+  });
+  return result;
+};
+
+getPendingAndErrorJobs().then(console.log);
